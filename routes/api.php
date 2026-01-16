80e56d4e2643b50b9fe68fe8ca04c6567196a815
brehehe <bratang1h@gmail.com>
2026-01-16 09:46:38 +0700

    Update

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,5 @@
 <?php
 
-use App\Http\Controllers\API\OneHealth\Auth\AuthController;
-use App\Http\Controllers\API\OneHealth\Consultation\ConsultationIndex;
-use App\Http\Controllers\API\OneHealth\Deployment\PatientController;
-use App\Http\Controllers\API\OneHealth\MasterData\RegionController;
-use App\Http\Controllers\API\Onehealth\Organization\OrganizationController;
-use App\Http\Controllers\API\OneHealth\OutPatient\EncounterController;
-use App\Http\Controllers\API\TestingController;
 use App\Http\Controllers\Api\RealTimeMetricsController;
 use App\Models\Exam\ExamLiveSession;
 use Illuminate\Http\Request;
@@ -169,65 +162,3 @@ Route::get('/metrics/system', [RealTimeMetricsController::class, 'getSystemMetri
 
 Route::get('/metrics/livestream', [RealTimeMetricsController::class, 'getLiveStreamMetrics'])
     ->name('api.metrics.livestream');
-
-Route::post('/access-token', [AuthController::class, 'accessToken']);
-
-Route::prefix('/master-data')->group(function () {
-    Route::get('/province', [RegionController::class, 'getProvince']);
-    Route::get('/city', [RegionController::class, 'getCity']);
-    Route::get('/district', [RegionController::class, 'getDistrict']);
-    Route::get('/sub-district', [RegionController::class, 'getSubDistrist']);
-});
-
-Route::prefix('/deployment')->group(function () {
-    Route::post('/create-patient', [PatientController::class, 'createPatient']);
-    Route::get('/get-patient/{patient_id}', [PatientController::class, 'getPatient']);
-});
-
-Route::prefix('/out-patient')->group(function () {
-    Route::post('/create-encounter', [EncounterController::class, 'createEncounter']);
-});
-
-Route::prefix('organization')->group(function () {
-    Route::post('/create', [OrganizationController::class, 'createOrganization']);
-});
-
-Route::prefix('/testing')->group(function () {
-    Route::prefix('/company')->group(function () {
-        Route::post('/post-put', [TestingController::class, 'postPutCompany']);
-        Route::get('/get', [TestingController::class, 'getCompany']);
-    });
-    Route::prefix('/location')->group(function () {
-        Route::post('/post-put', [TestingController::class, 'postPutLocation']);
-    });
-
-    Route::prefix('/practitiont')->group(function () {
-        Route::get('/get-by-nik', [TestingController::class, 'getPractitiont']);
-    });
-
-    Route::prefix('/patient')->group(function () {
-        Route::post('/post-put', [TestingController::class, 'postPutPatient']);
-        Route::get('/get-nik', [TestingController::class, 'getPatient']);
-    });
-
-    Route::prefix('/encounter')->group(function () {
-        Route::post('/post-put', [TestingController::class, 'postPutEncounter']);
-    });
-
-    Route::prefix('/condition')->group(function () {
-        Route::post('/post-put', [ConsultationIndex::class, 'postPutConsultation']);
-        Route::post('/postput', [TestingController::class, 'postPutCondition']);
-    });
-
-    Route::prefix('/medication')->group(function () {
-        Route::post('/post-put', [TestingController::class, 'postPutMedication']);
-    });
-
-    Route::prefix('/medication-request')->group(function () {
-        Route::post('/post-put', [TestingController::class, 'postPutMedicationRequest']);
-    });
-
-    Route::prefix('/medication-dispense')->group(function () {
-        Route::post('/post-put', [TestingController::class, 'postPutMedicationDispense']);
-    });
-});
