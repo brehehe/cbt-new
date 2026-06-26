922f30b097ca9369ef250e9c4742b87757c6051f
brehehe <bratang1h@gmail.com>
2026-06-26 20:02:12 +0700

    fix selectize wire:key

--- resources/views/livewire/admin/master/module/admin-master-module-modal.blade.php
+++ resources/views/livewire/admin/master/module/admin-master-module-modal.blade.php
@@ -131,10 +131,10 @@
                             @enderror
                         </div>
                         @if (!$is_all_study)
-                            <div wire:key="studys-select-wrapper">
+                            <div wire:key="studys-select-wrapper-{{ $data_id ?? 'new' }}">
                                 <label for="studys" class="block text-sm font-medium text-gray-700">Prodi <span
                                         class="text-red-600">*</span></label>
-                                <div wire:key="select-studys" class="w-full">
+                                <div wire:key="select-studys-{{ $data_id ?? 'new' }}" class="w-full">
                                     <select class="mt-1 form-control w-full" x-data x-ref="input" x-init="$($refs.input).selectize({
                                             dropdownParent: 'body',
                                             allowClear: true,
