ed45a72a7d3186f6706486450c52e80d4650f418
brehehe <bratang1h@gmail.com>
2026-06-26 18:29:45 +0700

    fix AdminDashboard typo

--- resources/js/Dashboard/AdminDashboard.jsx
+++ resources/js/Dashboard/AdminDashboard.jsx
@@ -1,4 +1,4 @@
-cimport React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 export default function AdminDashboard({ userProfile = {} }) {
